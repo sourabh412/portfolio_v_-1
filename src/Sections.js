e40827d35da74abca3fe5619e76be222381cd94c
sourabh <Hiremathsourabh12@gmail.com>
2022-10-04 20:31:18 +0530

    3

--- src/Sections.js
+++ src/Sections.js
@@ -1,18 +1,5 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 import About from './components/About';
-
-const up_anim = {
-    offscreen: {
-        position: "relative",
-        y: 300,
-        opacity: 0,
-    },
-    onscreen: {
-        y: 0,
-        opacity: 1
-    }
-}
 
 function Sections() {
     return (
@@ -20,42 +7,18 @@ function Sections() {
             <section className='h-auto mt-1 py-20' id="about">
                 <About />
             </section>
-            <motion.section className='h-screen' id="projects"
-                variants={up_anim}
-                initial="offscreen"
-                whileInView="onscreen"
-                viewport={{ once: false }}
-                transition={{ duration: 1 }}
-            >
+            <section className='h-screen' id="projects">
 
-            </motion.section>
-            <motion.section className='h-screen' id="benchmarks"
-                variants={up_anim}
-                initial="offscreen"
-                whileInView="onscreen"
-                viewport={{ once: false }}
-                transition={{ duration: 1 }}
-            >
+            </section>
+            <section className='h-screen' id="benchmarks">
 
-            </motion.section>
-            <motion.section className='h-screen' id="skills"
-                variants={up_anim}
-                initial="offscreen"
-                whileInView="onscreen"
-                viewport={{ once: false }}
-                transition={{ duration: 1 }}
-            >
+            </section>
+            <section className='h-screen' id="skills">
 
-            </motion.section>
-            <motion.section className='h-screen' id="contact"
-                variants={up_anim}
-                initial="offscreen"
-                whileInView="onscreen"
-                viewport={{ once: false }}
-                transition={{ duration: 1 }}
-            >
+            </section>
+            <section className='h-screen' id="contact">
 
-            </motion.section>
+            </section>
         </>
     )
 }
